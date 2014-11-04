324c1d0201b40fb034de45c7386a413192cdf90f
James Scott <jscott10@Jamess-MacBook-Air-2.local>
2014-11-04 00:56:04 -0500

    Cleaned up ChecklistItem to match Swift version tutorial

--- Checklists/ChecklistItem.swift
+++ Checklists/ChecklistItem.swift
@@ -8,40 +8,29 @@
 
 import Foundation
 
-// App crashes if ChecklistItem is not made a subclass of NSObject!!!
-
-/*
-From stackoverflow:
-NSKeyedArchiver will only work with Objective-C classes, not pure Swift classes. You can bridge your class to Obj-C by marking it with the @objc attribute or by inheriting from an Objective-C class, such as NSObject.
-
-NOTE: @objc doesn't seem to fix it!
-
-*/
-
 class ChecklistItem : NSObject, NSCoding {
-//@objc class ChecklistItem : NSCoding {
     
-    var text: String
-    var checked: Bool
+    var text = ""
+    var checked = false
     
-    init(text: String, checked: Bool)   {
-        self.text = text
-        self.checked = checked
+    override init()  {
+        super.init()
     }
     
-    required convenience init(coder aDecoder: NSCoder) {
-        var text = aDecoder.decodeObjectForKey("Text") as String
-        var checked = aDecoder.decodeBoolForKey("Checked") as Bool
-        self.init(text: text, checked: checked)
-    }
-    
-    func toggleChecked()  {
-        checked = !checked
+    required init(coder aDecoder: NSCoder) {
+        text = aDecoder.decodeObjectForKey("Text") as String
+        checked = aDecoder.decodeBoolForKey("Checked")
+        super.init()
     }
     
     func encodeWithCoder(aCoder: NSCoder)  {
         aCoder.encodeObject(text, forKey: "Text")
         aCoder.encodeBool(checked, forKey: "Checked")
     }
+    
+    func toggleChecked()  {
+        checked = !checked
+    }
+    
 }
 
